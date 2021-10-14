ea82f24bef74cd1aea33ba10e407fd77f48e32b4
facub <54050346+facub@users.noreply.github.com>
2021-10-14 12:59:02 -0300

    Page navegation added

--- Misterio/src/App.js
+++ Misterio/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import MainPage from './components/mainpage';
+import CreatingFrom from './components/formcreatingame';
+import  { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <MainPage/>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MainPage}></Route>
+        <Route exact path="/formcreatingame" component={CreatingFrom}></Route>
+      </Switch>
+    </Router>
   );
 }
 export default App;
