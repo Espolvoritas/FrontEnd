637a0b6ef843eb3716998314f39bca3b74f50b00
facub <54050346+facub@users.noreply.github.com>
2021-10-18 16:04:10 -0300

    From and List redirection integration

--- Misterio/src/App.js
+++ Misterio/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import MainPage from './components/mainpage';
 import CreatingFrom from './components/formcreatingame';
+import ListGames from './components/listofgames';
 import  { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={MainPage}></Route>
         <Route exact path="/formcreatingame" component={CreatingFrom}></Route>
+        <Route exact path="/listofgames" component={ListGames}></Route>
       </Switch>
     </Router>
   );
