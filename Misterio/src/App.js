48774f940ea6370992d71ded5a137f223b4419ad
facub <54050346+facub@users.noreply.github.com>
2021-10-18 15:43:26 -0300

    Form and Lobby integration

--- Misterio/src/App.js
+++ Misterio/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import MainPage from './components/mainpage';
+import CreatingFrom from './components/formcreatingame';
+import Lobby from './components/lobby';
+import GameBoard from './components/gameboard.js'
+import  { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <MainPage/>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MainPage}></Route>
+        <Route exact path="/formcreatingame" component={CreatingFrom}></Route>
+        <Route exact path="/lobby" component={Lobby}></Route>
+        <Route exact path="/gameboard" component={GameBoard}></Route>
+      </Switch>
+    </Router>
   );
 }
 export default App;
--- Misterio/src/components/formcreatingame.js
+++ Misterio/src/components/formcreatingame.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 import '../css/formcreatingame.css';
+import { useHistory } from "react-router-dom";
 
 const CreatingFrom = () => {
 
+    const [id, setId] = useState(0);
     const [name, setName] = useState("");
     const [host, setHost] = useState("");
+    const [isRepeated, setIsRepeated] = useState(false);
+    const history = useHistory();
 
-    const EPcreategame = async () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
         const gameData = {
             "name": name,
             "host": host
@@ -20,25 +25,42 @@ const CreatingFrom = () => {
             },
             body: JSON.stringify(gameData)
         })
+
+        setId(await response.json())
+
+        setIsRepeated(response.status === 400)
+
+        if (response.status === 201){
+            const state = {"gameId": id, "gameName": name, "gameHost": host }
+            history.push("/lobby", state);
+        }
+
     }
 
     return (
         <div className="Form">
-            <h1>Crear la partida</h1>
-            <form>
+            <h1 >Crear la partida </h1>
+            <form onSubmit={handleSubmit}>
+                {
+                    // Check if game name is repeated
+                    (isRepeated)
+                    ? <label> The game name is repeated </label> 
+                    : <p/> 
+                }
+                <p/>
                 <label>
                     Nombre de la partida
                     <p/>
-                    <input type="text" name="name" onChange={e => setName(e.target.value)} />
+                    <input type="text" name="name" autoComplete="off" placeholder="Nombre de partida" onChange={e => setName(e.target.value)} />
                 </label>
                 <p/>
                 <label>
                     Apodo
                     <p/>
-                    <input type="text" name="host"onChange={e => setHost(e.target.value)} />
+                    <input type="text" name="host" autoComplete="off" placeholder="Apodo" onChange={e => setHost(e.target.value)} />
                 </label>
                 <p/>
-                <input type="submit" value="Crear" onClick={EPcreategame}/>
+                <input type="submit" value="Crear" />
             </form>
         </div>
     );
