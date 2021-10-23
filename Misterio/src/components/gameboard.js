d191b1e29ea6f9c4086f6bb9ef4e030e394243b7
mc <mcittadini@bitbucket.org>
2021-10-23 14:11:20 -0300

    primeros avances para mostrar el turno actual

--- Misterio/src/components/gameboard.js
+++ Misterio/src/components/gameboard.js
@@ -1,16 +1,27 @@
-import React from "react";
+import {React, useState} from "react";
 import '../css/gameboard.css';
 import Dice from 'react-dice-roll';
 
 const GameBoard = () => {
+    const [actualTurn, setData] = useState("")
+
+    const ws2 = new WebSocket("ws://localhost:8000/gameboard/A DEFINIR")
+    ws2.onmessage = (event) => {
+        setData(JSON.parse(event.actualTurn));
+    };
+
     const ws = new WebSocket("ws://localhost:8000/gameboard/${userID}/rollDice")
-  
+
     return (
      <div className="background-image">
         <div className="DiceRoll">
             <Dice placement="botton-left" faceBg="black" size="70" onRoll={(value) => ws.send(value)}/>
         </div>
+        <span className="Turn">
+            <h1>Es el turno de {actualTurn}</h1>
+        </span>
     </div>
-    ) 
+    )
 }
 export default GameBoard;
+
