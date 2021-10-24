93131e3dcfc4ffc7f96be5748aef07ca809ece79
facub <54050346+facub@users.noreply.github.com>
2021-10-23 22:59:16 -0300

    Adding routes to pages

--- Misterio/src/App.js
+++ Misterio/src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './components/mainpage';
+import CreatingFrom from './components/formcreatingame';
+import './App.css';
 
 function App() {
   return (
-    <MainPage/>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MainPage}></Route>
+        <Route exact path="/formcreatingame" component={CreatingFrom}></Route>
+      </Switch>
+    </Router>
   );
 }
 export default App;
