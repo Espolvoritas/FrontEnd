6368e164bf9e3cbdf10c0aa99c79972550861101
mc <mcittadini@bitbucket.org>
2021-10-25 12:17:16 -0300

    popup css enhancement

--- Misterio/src/components/listofgames.js
+++ Misterio/src/components/listofgames.js
@@ -44,9 +44,11 @@ const ListGames = () => {
         }
     }
 
-    const [nickname, setNickName] = useState(false);
+    const [nickname, setNickName] = useState("");
     const [gameid, setGameID] = useState(-1);
     const [isRepeated, setIsRepeated] = useState(false);
+    const [notFormated, setNotFormated] = useState(false);
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -54,28 +56,32 @@ const ListGames = () => {
             "gameId": gameid,
             "playerNickname": nickname
         }
-        try {
-            const joinChecked = await fetch('http://127.0.0.1:8000/game/joinCheck', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                },
-                body: JSON.stringify(nickNameData)
-            })
-            console.log(joinChecked)
-            const response = await joinChecked.json()
-            if (joinChecked.status === 200 && response["nicknameIsValid"]){
-                const state = {"player_id": response["playerId"]}
-                history.push("/lobby", state);
-            } else {
-                setIsRepeated(!response["nicknameIsValid"])
-                console.log("Error en joinChecked")
+        if ((nickname !== "") && (nickname.length < 21) && (nickname.length > 4)){
+            try {
+                const joinChecked = await fetch('http://127.0.0.1:8000/game/joinCheck', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    body: JSON.stringify(nickNameData)
+                })
+                console.log(joinChecked)
+                const response = await joinChecked.json()
+                if (joinChecked.status === 200 && response["nicknameIsValid"]){
+                    const state = {"player_id": response["playerId"]}
+                    history.push("/lobby", state);
+                } else {
+                    setIsRepeated(!response["nicknameIsValid"])
+                    console.log("Error en joinChecked")
+                }
             }
-        }
-        catch(error) {
-            console.log(error)
+            catch(error) {
+                console.log(error)
+            }
+        } else {
+            setNotFormated(true)
         }
     }
 
@@ -83,6 +89,7 @@ const ListGames = () => {
         console.log(listGame[i]["id"])
         setGameID(listGame[i]["id"])
         setIsRepeated(false)
+        setNotFormated(false)
     }
 
     // process and display the received json
@@ -115,18 +122,24 @@ const ListGames = () => {
                                 {close => (
                                     <div className="modal">
                                         <button className="close" onClick={close}>&times;</button>
-                                        <div className="popupheader"> Ingrese su apodo </div>
+                                        <div className="popupheader"> Ingrese un apodo entre 5 y 20 caracteres </div>
                                         <div className="content">
                                             {
                                                 (isRepeated)
                                                 ? <label className="takensign"> Apodo tomado </label>
                                                 : <p/>
                                             }
+                                            {
+                                                (notFormated) 
+                                                ?  <label className="takensign"> Apodo mal formateado </label>
+                                                : <p/>
+                                            }
                                             <form>
                                                 <label>
                                                     <p/>
-                                                     <input className="nicknameinput" type="text" name="nickname" onClick={() => setIsRepeated(false)}
-                                                        onChange={e => setNickName(e.target.value)}/>
+                                                    <input className="nicknameinput" type="text" name="nickname"  
+                                                        onClick={() => {setIsRepeated(false); setNotFormated(false)}}
+                                                        onChange={e => setNickName(e.target.value)} required autoComplete = "off"/>
                                                 </label>
                                                 <input className="sendbutton" type="submit" value="->" onClick={handleSubmit}/>
                                             </form>
@@ -145,7 +158,8 @@ const ListGames = () => {
     return (
         <div className="ListGames">
             <h1>Selecciona la partida</h1>
-            {(isEmptyList) ? <p className="Warning">⚠️No hay partidas disponibles. Prueba actualizar o crear una partida⚠️</p> : <p/>}
+            {(isEmptyList) ? <p className="Warning">⚠️No hay partidas disponibles. 
+            Prueba actualizar o crear una partida⚠️</p> : <p/>}
             {DisplayJson()}
         </div>
     );
