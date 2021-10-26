cf0b4279ab1bb7912abf774a3515a0a453353905
mc <mcittadini@bitbucket.org>
2021-10-25 21:21:08 -0300

    changes in functionallity of gameboard and removal of files.

--- Misterio/src/components/gameboard.js
+++ Misterio/src/components/gameboard.js
@@ -1,28 +1,35 @@
-import {React, useState} from "react";
+import {React, useState, useRef, useEffect} from "react";
 import '../css/gameboard.css';
 import Dice from 'react-dice-roll';
+import { useHistory } from "react-router-dom";
 
 const GameBoard = () => {
+
+    const history = useHistory()
+    const datahost = history.location.state
+    const ws = useRef(null);
+
     const [actualTurn, setData] = useState("")
 
-    const ws2 = new WebSocket("ws://localhost:8000/gameboard/A DEFINIR")
-    ws2.onmessage = (event) => {
-        setData(JSON.parse(event.actualTurn));
-    };
-
-    const ws = new WebSocket("ws://localhost:8000/gameboard/${userID}/rollDice")
+    useEffect(() => {
+        ws.current = new WebSocket("ws://localhost:8000/gameBoard/" 
+                                + String(datahost["player_id"]) + "/rollDice")
+        ws.current.onmessage = (event) => {
+            setData(JSON.parse(event.data));
+        };
+    }, []);
 
     return (
-     <div className="background-image">
+    <div className="background-image">
         <div className="DiceRoll">
-            <Dice placement="botton-left" faceBg="black" size="70" onRoll={(value) => ws.send(value)}/>
+            <Dice placement="botton-left" faceBg="black" size="70" 
+                onRoll={(value) => ws.current.send(value)}/>
         </div>
         <div className="Turn">
             <h1>Esta jugando el detective: {actualTurn}</h1>
         </div>
     </div>
-    )
+    );
 }
 
 export default GameBoard;
-
