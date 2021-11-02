50c90841677ecf9f5784344415d59a53f0298cd1
Sofia Bobbiesi <sofia.bobbiesi@mi.unc.edu.ar>
2021-11-02 15:06:11 -0300

    cards import

---
+++ Misterio/src/components/cards-img.js
@@ -0,0 +1,29 @@
+const CardsImg = {
+    //Monsters
+    'Dr. Jekyll Mr. Hyde': require('../media/cards/Jekyll_hyde.png').default,
+    'Dracula': require('../media/cards/dracula.png').default,
+    'Fantasma': require('../media/cards/fantasma.png').default,
+    'Frankenstein': require('../media/cards/frankenstein.png').default,
+    'Hombre Lobo': require('../media/cards/hombre_lobo.png').default,
+    'Momia': require('../media/cards/momia.png').default,
+    //Victims
+    'Ama de Llaves': require('../media/cards/ama_de_llaves.png').default,
+    'Conde': require('../media/cards/conde.png').default,
+    'Condesa': require('../media/cards/condesa.png').default,
+    'Doncella': require('../media/cards/doncella.png').default,
+    'Jardinero': require('../media/cards/jardinero.png').default,
+    'Mayordomo': require('../media/cards/mayordomo.png').default,
+    //Places
+    'Alcoba': require('../media/cards/alcoba.png').default,
+    'Biblioteca': require('../media/cards/biblioteca.png').default,
+    'Bodega': require('../media/cards/bodega.png').default,
+    'Cochera': require('../media/cards/cochera.png').default,
+    'Laboratorio': require('../media/cards/laboratorio.png').default,
+    'Panteon': require('../media/cards/panteon.png').default,
+    'Salon': require('../media/cards/salon.png').default,
+    'Vestibulo': require('../media/cards/vestibulo.png').default,
+    //Salem Witch
+    'Bruja de Salem': require('../media/cards/bruja_salem.png').default
+}
+
+export default CardsImg;
