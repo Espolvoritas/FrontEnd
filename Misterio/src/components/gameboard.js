371f761f04c485523bbfb4e90c3bb049ab776f3d
Sofia Bobbiesi <sofia.bobbiesi@mi.unc.edu.ar>
2021-11-08 15:15:38 -0300

    endpoint completed and working

--- Misterio/src/components/gameboard.js
+++ Misterio/src/components/gameboard.js
@@ -1,35 +1,88 @@
 import {React, useState, useRef, useEffect} from "react";
-import '../css/gameboard.css';
 import Dice from 'react-dice-roll';
-import { useHistory } from "react-router-dom";
-import {Suspicion, ShowSuspicion, ChooseCard} from "./suspicion"
+import {useHistory} from "react-router-dom";
+import Cards from "./cards";
+import Rules from "./rules";
+import {Suspicion, ShowSuspicionResult, ChooseCard, ShowStatus, NotifySend, NotifySuspicion} from "./suspicion"
+import { emitCustomEvent } from 'react-custom-events';
+import { useCustomEventListener } from 'react-custom-events';
 
 const GameBoard = () => {
 
     const history = useHistory()
     const datahost = history.location.state
     const ws = useRef(null);
-
-    const [actualTurn, setData] = useState("")
+    const [currentPlayer, setTurn] = useState("")
+    const [cards, setCards] = useState([])
+    const isPlaying = (datahost["player_name"] === currentPlayer)
+    const [message, setMessage] = useState("")
+    const [card, setCard] = useState("");
+    const [accused, setAccused] = useState("");
+    let arriveSus = useRef(false)
 
     useEffect(() => {
-        ws.current = new WebSocket("ws://localhost:8000/gameBoard/" 
-                                + String(datahost["player_id"]) + "/rollDice")
+        ws.current = new WebSocket("ws://localhost:8000/gameBoard/"
+                                + String(datahost["player_id"]))
         ws.current.onmessage = (event) => {
-            setData(JSON.parse(event.data));
+            emitCustomEvent('websocket', JSON.parse(event.data));
+            if(JSON.parse(event.data)["code"] & 1)
+                setTurn(JSON.parse(event.data)["currentPlayer"]);
+            if (JSON.parse(event.data)["code"] & 2)
+                setCards(JSON.parse(event.data)["cards"]);
+            if (JSON.parse(event.data)["code"] & 8){
+                arriveSus = true
+            }
         };
-    }, []);
+    }, [])
+
+    console.log(arriveSus)
+
+    async function handleDice(value) {
+        const diceValue = {
+            "playerId": datahost["player_id"], 
+            "roll": value
+        }
+        const response = await fetch('http://127.0.0.1:8000/gameBoard/rollDice', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(diceValue)   
+        })
+    }
+
+    useCustomEventListener('res-sus', resSus => {
+        setAccused(resSus[0])
+        setCard(resSus[1])
+    })
 
     return (
-    <div className="background-image">
-        <div className="DiceRoll">
-            <Dice placement="botton-left" faceBg="black" size="70" 
-                onRoll={(value) => ws.current.send(value)}/>
+        <div className="background-image">
+            <div className="DiceRoll">
+                <Dice placement="botton-left" faceBg="black" size="70"  
+                    disabled={!isPlaying} onRoll={(value) => handleDice(value)}/>
+            </div>
+            <div className="Turn">
+                <h1>Esta jugando el detective: {currentPlayer}</h1>
+            </div>
+            <div>
+                {(datahost["player_id"] === 1) ? Suspicion(13,1) : <b/> }
+                {NotifySuspicion()}
+                {arriveSus ? ChooseCard(ws.current): <b/> }
+                {ShowStatus()}
+                {NotifySend()}
+                {(accused !== "" && card !== "") ? ShowSuspicionResult(accused, card) : <b/>}
+                {(message === "") ? <b/> : <Rules message={message}/>}
+            </div>
+            <div>
+                {Rules()}
+            </div>
+            <div>
+                {Cards(cards)}
+            </div>
         </div>
-        <div className="Turn">
-            <h1>Esta jugando el detective: {actualTurn}</h1>
-        </div>
-    </div>
     );
 }
 
--- Misterio/src/components/suspicion.js
+++ Misterio/src/components/suspicion.js
@@ -1,63 +1,113 @@
 import React, { useState } from "react";
 import Popup from "reactjs-popup";
-import CardsImg from "./cards-img";
+import {CardsImg, CardsName} from "./cardReference";
+import { useCustomEventListener } from 'react-custom-events';
+import { emitCustomEvent } from 'react-custom-events';
 
 
-function Suspicion(place) {
+function Suspicion(place, playerID) {
 
-    const [victim, setVictim] = useState("");
-    const [monster, setMonster] = useState("");
-    const validInput = victim === "" || monster === "";
-    /* const [place, setPlace] = useState(""); */
+    const [victim, setVictim] = useState(0);
+    const [monster, setMonster] = useState(0);
+    const validInput = victim !== 0 && monster !== 0;
 
-
-    const handleSubmit = async (e) => {
+    const handleSubmit = async (e) => {  //SE LLAMA CUANDO SE CIERRA LA POPUP
         e.preventDefault();
-        console.log(victim, monster, place);
-
-
-
+        /* emitCustomEvent('suspicionArrived', true); */
+        const sus = {
+            "playerId": playerID,
+            "victimId": parseInt(victim),
+            "culpritId": parseInt(monster)
+        }
+        const response = await fetch('http://127.0.0.1:8000/gameBoard/checkSuspicion', {
+                  method: 'POST',
+                  headers: {
+                      'Accept': 'application/json',
+                      'Content-Type': 'application/json',
+                      'Access-Control-Allow-Origin': '*'
+                  },
+                  body: JSON.stringify(sus)
+                  
+        })
+        const res = await response.json();
+        emitCustomEvent('res-sus', [res["responsePlayer"],res["suspicionCard"]]);
     }
     return(
-        <Popup 
-        modal
+        <Popup
         open={true}
+        modal
         onClose={handleSubmit}
         closeOnDocumentClick = {false}
         >
         {close => (
-            <div className="suspicion-dropdown">
-                <button className="close-sus" onClick={close}>&times;</button> <br/>
-                <div className="header-sus">Realiza una sospecha</div>
+            <div className="suspicion-box">
+                <h3 className="header-sus">Realiza una sospecha</h3>
 
-                <select onChange={(e) => setMonster(e.target.value)}>
-                    <option value="" selected disabled hidden>Elige un monstruo</option>
-                    <option value={6}>Dr. Jekyll Mr. Hyde</option>
-                    <option value={1}>Drácula</option>
-                    <option value={4}>Fantasma</option>
-                    <option value={2}>Frankenstein</option>
-                    <option value={3}>Hombre Lobo</option>
-                    <option value={5}>Momia</option>
-                </select>
-                asesinó al/a la
-                <select onChange={(e) => setVictim(e.target.value)}>
-                    <option value="" selected disabled hidden>Elige una víctima</option>
-                    <option value={9}>Ama de Llaves</option>
-                    <option value={7}>Conde</option>
-                    <option value={8}>Condesa</option>
-                    <option value={11}>Doncella</option>
-                    <option value={12}>Jardinero</option>
-                    <option value={10}>Mayordomo</option>
-                </select>
-                en el/la {place}
-                <input disabled={validInput} className="send-sus" type="submit" value="➤" onClick={close}/>
+                <div className="suspicion-dropdown">
+                    <select onChange={(e) => setMonster(e.target.value)}>
+                        <option value="" selected disabled hidden>Elige un monstruo</option>
+                        <option value={6}>Dr. Jekyll Mr. Hyde</option>
+                        <option value={1}>Drácula</option>
+                        <option value={4}>Fantasma</option>
+                        <option value={2}>Frankenstein</option>
+                        <option value={3}>Hombre Lobo</option>
+                        <option value={5}>Momia</option>
+                    </select>
+                    asesinó al/a la
+                    <select onChange={(e) => setVictim(e.target.value)}>
+                        <option value="" selected disabled hidden>Elige una víctima</option>
+                        <option value={9}>Ama de Llaves</option>
+                        <option value={7}>Conde</option>
+                        <option value={8}>Condesa</option>
+                        <option value={11}>Doncella</option>
+                        <option value={12}>Jardinero</option>
+                        <option value={10}>Mayordomo</option>
+                    </select>
+                    en el/la {CardsName[place]}
+                    {(validInput) ? <input className="send-sus" type="submit" value="➤" onClick={close}/> : <b/>}
+                </div>
+            </div>
+        )}
+        </Popup>
+    )
+}
+// TODO: hacerle clases propias
+function NotifySuspicion(){
+    
+    const [player, setPlayer] = useState(0);
+    const [victim, setVictim] = useState(0);
+    const [monster, setMonster] = useState(0);
+    const [place, setPlace] = useState(0);
+
+    useCustomEventListener('websocket', data => {
+        console.log("hola susana")
+        if ((data["code"] & 4)){
+            setPlayer(data["currentPlayer"]);
+            setVictim(data["victim"]);
+            setMonster(data["culprit"]);
+            setPlace(data["room"]);
+        }
+    });
+
+    if (victim === 0 || monster === 0) return
+
+    return(
+        <Popup 
+        modal
+        open={true}
+        closeOnDocumentClick={false}
+        >
+        {close => (
+            <div className="show-status">
+                <button className="close-show" onClick={close}>✖</button> <br/>
+            <div className="header-sus">{player} sospechó de {CardsName[monster]}, {CardsName[victim]} en {CardsName[place]}</div>
             </div>
         )}
         </Popup>
     )
 }
 
-function ShowSuspicion(nickname, cardname) {
+function ShowSuspicionResult(nickname, card) {
     return(
         <Popup 
         modal
@@ -67,46 +117,70 @@ function ShowSuspicion(nickname, cardname) {
             <div className="show-sus">
                 <button className="close-show" onClick={close}>✖</button> <br/>
                 <div className="header-sus">{nickname} tiene esta carta</div>
-                <img className="card-sus" src={CardsImg[cardname]} alt="sus-answer"/>
+                <img className="card-sus" src={CardsImg[card]} alt={CardsName[card]}/>
             </div>
         )}
         </Popup>
     )
 }
 
-
-function ChooseCard(deck){
+function ChooseCard(ws){
+    const [deck, setDeck] = useState("")
     const [pickedCard, setPickedCard] = useState(0);
-    const validInput = pickedCard === 0;
+    const validInput = pickedCard !== 0;
 
-    const handleCard = async (e) => {
-        e.preventDefault();
+    useCustomEventListener('websocket', data => {
+        if ((data["code"] & 8)){
+            setDeck(data["matchingCards"]);
+        }
+    });
+
+    const handleCard = () => {
+        ws.send(JSON.stringify({'code': 'PICK_CARD', 'card': pickedCard}));
         console.log(pickedCard);
     }
 
-    return(
-        <div>
-            <Popup 
-            modal
-            open={true}
-            onClose={handleCard}
-            closeOnDocumentClick={false}
-            >
-                {close => (
-                    <div className="choose-card">
-                        <div className="header-sus">Elige una carta para mostrar</div>
-                        {deck.map((card, i) => (
-                            <img key={i} className="card-pick" src={CardsImg[card]} alt="pickable-card" onClick={() => setPickedCard(card)}/>
-                        ))}
-                       <br/><input disabled={validInput} className="send-card" type="submit" value="Enviar" onClick={close}/>
-                    </div>
-                )}
-            </Popup>
-        </div>
-    )
+    if (deck.length <= 1) return 
+
+        return(
+            <div>
+                <Popup 
+                modal
+                open={true}
+                onClose={handleCard}
+                closeOnDocumentClick={false}
+                >
+                    {close => (
+                        <div className="choose-card">
+                            <div className="header-sus">Elige una carta para mostrar</div>
+                            {Object.keys(deck).map((i) => (
+                                <img key={i} className="card-pick" src={CardsImg[deck[i]]} alt={CardsName[deck[i]]}
+                                    onClick={() => setPickedCard(deck[i])}/>
+                            ))}
+                            {(validInput) ? <div className="notice-card">Elegiste esta carta: {CardsName[pickedCard]}<br/>
+                                <input className="send-card" type="submit" value="Enviar" onClick={close}/></div>: <b/>}
+                        </div>
+                    )}
+                </Popup>
+            </div>
+        )
 }
 
-function ShowStatus(nickname, hasCard){
+function ShowStatus(){
+    const [hasCard, setHasCard] = useState(false);
+    const [nickname, setNickname] = useState("");
+    const [sus, setSus] = useState("");
+
+    useCustomEventListener('websocket', data => {
+        if (data["code"] & 16){
+            setHasCard(data["responded"])
+            setNickname(data["responsePlayer"])
+            setSus(data["suspicionPlayer"])
+        }
+    });
+
+    if (nickname === "" || sus === "") return
+
     return(
         <Popup 
         modal
@@ -116,12 +190,42 @@ function ShowStatus(nickname, hasCard){
         {close => (
             <div className="show-status">
                 <button className="close-status" onClick={close}>✖</button> <br/>
-                {(hasCard) ? <div className="notice-card">{nickname} respondió a la sospecha</div> 
-                    : <div className="notice-card">{nickname} no tiene cartas para responder</div>}
+                {(hasCard) ? <div className="notice-card">{nickname} respondió a la sospecha a {sus}</div> 
+                    : <div className="notice-card">{nickname} no tiene cartas para responderle a {sus}</div>}
             </div>
         )}
         </Popup>
     )
 }
 
-export {Suspicion, ShowSuspicionResult, ChooseCard, ShowStatus}
+function NotifySend(){
+    const [card, setCard] = useState(0);
+    const [nickname, setNickname] = useState("");
+
+    useCustomEventListener('websocket', data => {
+        if (data["code"] & 32){
+            setCard(data["card"])
+            setNickname(data["suspicionPlayer"])
+        }
+    });
+
+    if (nickname === "" || card === 0) return
+    
+    return(
+        <Popup 
+        modal
+        open={true}
+        closeOnDocumentClick={false}
+        >
+        {close => (
+            <div className="notify-send">
+                <button className="close-notify" onClick={close}>✖</button> <br/>
+                <div className="header-sus">Le enviaste esta carta a {nickname}</div>
+                <img className="card-notify" src={CardsImg[card]} alt={CardsName[card]}/>
+            </div>
+        )}
+        </Popup>
+    )
+}
+
+export {Suspicion, NotifySuspicion, ShowSuspicionResult, ChooseCard, ShowStatus, NotifySend}
