512d430472b1d713981b7a3d2de7eebadeec0896
facub <54050346+facub@users.noreply.github.com>
2021-11-08 15:13:09 -0300

    Simple testing

---
+++ Misterio/src/tests/gameboard.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent, screen } from '@testing-library/react'
+import GameBoard from '../components/gameboard'
+import {createMemoryHistory} from 'history'
+import { Router } from "react-router-dom";
+
+test('renders without crashing', async () => {
+
+    const history = createMemoryHistory();
+    const state = { "player_name": "Jugador1" }
+    history.push("/Lobby", state);
+
+    render(
+    <Router history={history}>
+        <GameBoard />
+    </Router>
+    );
+
+    const linkElement = screen.getByText(/Esta jugando el detective: Jugador1/i)
+    expect(linkElement).toBeInTheDocument();
+})
