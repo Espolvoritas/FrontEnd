ed34be7486e54a582530a9724a12755c69a6adfc
Sofia Bobbiesi <sofia.bobbiesi@mi.unc.edu.ar>
2021-11-14 12:08:22 -0300

    add name on mouse hover

--- Misterio/src/components/cards.js
+++ Misterio/src/components/cards.js
@@ -1,7 +1,7 @@
 import {React, useState} from "react";
 import PerfectScrollbar from 'react-perfect-scrollbar'
 import 'react-perfect-scrollbar/dist/css/styles.css';
-import CardsImg from "./cardReference";
+import {CardsImg, CardsName} from "./cardReference";
 import { useCustomEventListener } from 'react-custom-events';
 
 const Cards = () => {
@@ -16,7 +16,8 @@ const Cards = () => {
     return(
         <PerfectScrollbar className = "gridContainer">
             {Object.keys(cards).map((i) => (
-                <img key={i} className = "cardsImage" src={CardsImg[cards[i]]} alt=""/>
+                <img key={i} className = "cardsImage" src={CardsImg[cards[i]]} 
+                    alt={CardsName[cards[i]]} title={CardsName[cards[i]]}/>
             ))}
         </PerfectScrollbar>
     );
