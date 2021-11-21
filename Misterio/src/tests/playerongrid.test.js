fc5cb98ffa9b49dfa26cbd561d77eefb106ede5e
facub <54050346+facub@users.noreply.github.com>
2021-11-21 13:01:34 -0300

    Minor changes testing

--- Misterio/src/tests/playerongrid.test.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
-import { render, screen } from '@testing-library/react'
-import PlayerOnGrid from '../components/playerongrid'
